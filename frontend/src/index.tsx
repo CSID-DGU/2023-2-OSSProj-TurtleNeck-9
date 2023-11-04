e54140d6f23c46a5a03acc7581e07e11bd77f3db
jjm2317 <wlaks2317@gmail.com>
2023-11-04 23:50:05 +0900

    feat: add mui

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,8 @@
 import { createRoot } from 'react-dom/client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root') as HTMLElement;
 
