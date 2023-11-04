7cf6dac4e6b08e4da283aa177a804b3afdbbeeb4
Seongjoon-K <jobcho6320@naver.com>
2023-11-04 23:11:05 +0900

    feat: 유저 엔티티에 전공 FK 값 추가

--- backend/src/main/java/ossproj/demo/entity/User.java
+++ backend/src/main/java/ossproj/demo/entity/User.java
@@ -1,7 +1,4 @@
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,17 +6,20 @@ import org.springframework.data.annotation.Id;
 
 @Getter
 @Entity
-@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column
     private Long studentId;
 
     @Column(nullable = false, unique = false)
     private String name;
+
+    @Column(name="major_id")
+    @OneToMany
+    private Long majorId;
 
     @Column(length = 100)
     private String password;
