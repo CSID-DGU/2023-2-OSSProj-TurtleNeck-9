900e68330a62bd158abd8f45d17bd40dc4275235
jjm2317 <wlaks2317@gmail.com>
2023-11-05 00:08:39 +0900

    feat: config tslint setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
+import { createRoot } from 'react-dom/client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-const container = document.getElementById('root') as HTMLElement
+const container = document.getElementById('root') as HTMLElement;
 
-const root = createRoot(container)
+const root = createRoot(container);
 
-root.render(<h1>Hello, World!1111</h1>)
+root.render(<h1>Hello, World!1111</h1>);
