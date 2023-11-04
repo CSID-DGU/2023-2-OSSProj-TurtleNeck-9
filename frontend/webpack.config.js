4b4f8d6bf1fb189151df6ab1ea0f04f8346b05c7
jjm2317 <wlaks2317@gmail.com>
2023-11-04 23:12:42 +0900

    build: add basic loader/plugin

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
+  mode: 'development',
   entry: {
-    main: './src/index.jsx'
+    main: './src/index.tsx'
   },
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist')
   },
@@ -16,7 +20,18 @@ module.exports = {
         test: /\.(tsx|ts|jsx|js)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      }
+      },
+      // 1.import 가능 -> 2.모듈이 실행될 때 html 파일의 <head>에 문자열화 된 CSS가 <style>태그로 삽입
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+
+  ]
 }
