a812abdcb969c245d1ccd6d413bfd59f26af8423
jjm2317 <wlaks2317@gmail.com>
2023-11-05 13:45:38 +0900

    feat: add default header

---
+++ frontend/src/components/Header.stories.tsx
@@ -0,0 +1,23 @@
+import type { StoryObj } from '@storybook/react';
+import { mockLectures } from '../types/lecture';
+
+import Header from './Header';
+
+const meta = {
+  title: 'Header',
+  component: Header,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    lectureList: mockLectures,
+  },
+};
