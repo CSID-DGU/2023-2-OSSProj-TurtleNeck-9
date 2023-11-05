381d4ed50ca3cd2cf3a7a3c88c37fc08994e4b05
Seongjoon-K <jobcho6320@naver.com>
2023-11-05 19:04:42 +0900

    refactor: 유저 레포지토리 파일 삭제

--- backend/src/main/java/ossproj/demo/entity/Major.java
+++ backend/src/main/java/ossproj/demo/entity/Major.java
@@ -16,6 +16,7 @@ public class Major {
     private Long id;
 
     private String name;
+
     private String department;
 
 }
--- backend/src/main/java/ossproj/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-
-import jakarta.persistence.Entity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ossproj.demo.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
