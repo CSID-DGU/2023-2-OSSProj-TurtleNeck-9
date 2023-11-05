2f7838be2b1409ed5220d4c612616400fb139714
jjm2317 <wlaks2317@gmail.com>
2023-11-05 13:29:08 +0900

    feat: init react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,9 +3,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const container = document.getElementById('root') as HTMLElement;
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+]);
+
 const root = createRoot(container);
 
-root.render(<h1>Hello, World!1111</h1>);
+root.render(<RouterProvider router={router} />);
