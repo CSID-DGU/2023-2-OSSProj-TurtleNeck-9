2d3a849f6b3141943face173247ed2eedafef9e6
Seongjoon-K <jobcho6320@naver.com>
2023-11-23 01:30:53 +0900

    refactor: 메ㅔ실행파일명 수정

---
+++ backend/src/test/java/ossproj/demo/RecommendedApplication.java
@@ -0,0 +1,13 @@
+package ossproj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendedApplication.class, args);
+	}
+
+}
