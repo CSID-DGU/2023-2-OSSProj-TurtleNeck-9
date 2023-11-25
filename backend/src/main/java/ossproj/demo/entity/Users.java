dad4b521021227cfb04625a10967ca1216d8b95f
Seongjoon-K <jobcho6320@naver.com>
2023-11-25 17:27:27 +0900

    refactor: studentId 로 필드명 수정 및 회원가입시 role 추가

--- backend/src/main/java/ossproj/demo/entity/Users.java
+++ backend/src/main/java/ossproj/demo/entity/Users.java
@@ -1,25 +1,36 @@
 package ossproj.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "users")
-public class Users {
+public class Users implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", unique = true, nullable = false)
     private Long userId;
 
     @Column(length = 15, unique = true, nullable = false)
-    private int studentNumber;
+    private String studentId;
 
-    @Column(nullable = false, unique = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @ManyToOne
@@ -30,10 +41,42 @@ public class Users {
     private String password;
 
     @Builder
-    public Users(int studentNumber, Major major, String username, String password) {
-        this.studentNumber = studentNumber;
+    public Users(String studentId, Major major, String username, String password) {
+        this.studentId = studentId;
         this.major = major;
         this.username = username;
         this.password = password;
     }
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
