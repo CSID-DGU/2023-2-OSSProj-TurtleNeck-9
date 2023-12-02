f9de76e7f281509aab3787e7d2357b9b001179fa
jjm2317 <wlaks2317@gmail.com>
2023-12-02 22:42:40 +0900

    feat: add mockup data

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,18 +14,21 @@ const Home = () => {
         <Box display="flex" height="400px" justifyContent="space-around" flexDirection="column" alignItems="center">
           <Typography variant="h5">추천시간표 조회</Typography>
           <Box display="flex" justifyContent="space-around" width="100%">
-            <MediaCard title="추천 시간표 1" description="18학점">
+            <MediaCard title="추천 시간표 1" description="산업시스템공학과 18학점">
               <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(1)}>
                 시간표 확인하기
               </Button>
             </MediaCard>
-
-            <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(2)}>
-              시간표 2
-            </Button>
-            <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(3)}>
-              시간표 3
-            </Button>
+            <MediaCard title="추천 시간표 2" description="산업시스템공학과 18학점">
+              <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(2)}>
+                시간표 확인하기
+              </Button>
+            </MediaCard>
+            <MediaCard title="추천 시간표 3" description="산업시스템공학과 18학점">
+              <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(3)}>
+                시간표 확인하기
+              </Button>
+            </MediaCard>
           </Box>
         </Box>
       </ResponsiveDrawer>
