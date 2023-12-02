74c3b412d990cbb31e55910a87fcdbc391972bd4
Seongjoon-K <jobcho6320@naver.com>
2023-12-03 00:04:04 +0900

    chore: 패키지 이동

--- backend/src/main/java/ossproj/demo/service/UserService.java
+++ backend/src/main/java/ossproj/demo/service/UserService.java
@@ -1,7 +1,7 @@
 package ossproj.demo.service;
 
 import ossproj.demo.dto.JwtToken;
-import ossproj.demo.dto.request.SignupRequest;
+import ossproj.demo.dto.request.auth.SignupRequest;
 import ossproj.demo.dto.response.SignupResponse;
 
 public interface UserService {
