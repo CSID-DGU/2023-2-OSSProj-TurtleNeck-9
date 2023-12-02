3624edf8b4af5bc9583b15648341c681f1884289
jjm2317 <wlaks2317@gmail.com>
2023-12-02 22:30:26 +0900

    feat: apply responsive drawer to home

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Container, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import Header from '../components/layout/Header';
+import ResponsiveDrawer from '../components/ResponsiveDrawer';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -9,21 +9,22 @@ const Home = () => {
   };
   return (
     <Container>
-      <Header />
-      <Box display="flex" height="400px" justifyContent="space-around" flexDirection="column" alignItems="center">
-        <Typography variant="h5">추천시간표 조회</Typography>
-        <Box display="flex" justifyContent="space-around" width="100%">
-          <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(1)}>
-            시간표 1
-          </Button>
-          <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(2)}>
-            시간표 2
-          </Button>
-          <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(3)}>
-            시간표 3
-          </Button>
+      <ResponsiveDrawer>
+        <Box display="flex" height="400px" justifyContent="space-around" flexDirection="column" alignItems="center">
+          <Typography variant="h5">추천시간표 조회</Typography>
+          <Box display="flex" justifyContent="space-around" width="100%">
+            <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(1)}>
+              시간표 1
+            </Button>
+            <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(2)}>
+              시간표 2
+            </Button>
+            <Button size="medium" variant="contained" color="warning" onClick={handleButtonClick(3)}>
+              시간표 3
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      </ResponsiveDrawer>
     </Container>
   );
 };
