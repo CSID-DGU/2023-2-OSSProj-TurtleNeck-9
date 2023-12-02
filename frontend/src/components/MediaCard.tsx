5c01b6d8070f7bf0aebbb23198272fc9f28fb922
jjm2317 <wlaks2317@gmail.com>
2023-12-02 22:41:31 +0900

    feat: define props

--- frontend/src/components/MediaCard.tsx
+++ frontend/src/components/MediaCard.tsx
@@ -1,32 +1,26 @@
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 type Props = {
   title: string;
+  description: string;
 };
 
-const MediaCard: FC<Props> = () => {
+const MediaCard: FC<PropsWithChildren<Props>> = ({ title, description, children }) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia sx={{ height: 140 }} image="/static/images/cards/contemplative-reptile.jpg" title="green iguana" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          추천 시간표 1
+          {title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents
-          except Antarctica
+          {description}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>{children}</CardActions>
     </Card>
   );
 };
