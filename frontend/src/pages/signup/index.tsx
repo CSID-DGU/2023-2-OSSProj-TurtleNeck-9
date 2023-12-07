41caa4146aa094a2a318087e2748dbdb9e285526
jjm2317 <wlaks2317@gmail.com>
2023-12-07 22:18:09 +0900

    feat: create signup page

---
+++ frontend/src/pages/signup/index.tsx
@@ -0,0 +1,94 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { Link, useParams } from 'react-router-dom';
+import ResponsiveDrawer from '../../components/ResponsiveDrawer';
+import { useLectureListQuery } from '../../query/lecture';
+
+const Signup = () => {
+  return (
+    <Container>
+      <ResponsiveDrawer>
+        <CssBaseline />
+        <div>
+          <Typography>회원가입</Typography>
+          <form noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+              Sign Up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link to="/signin">Already have an account? Sign in</Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </ResponsiveDrawer>
+    </Container>
+  );
+};
+
+export default Signup;
