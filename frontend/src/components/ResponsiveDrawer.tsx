2876ccc0df2e907833e75bd281c7131e03264e61
jjm2317 <wlaks2317@gmail.com>
2023-12-09 21:32:23 +0900

    feat: integrate session state to profile menu

--- frontend/src/components/ResponsiveDrawer.tsx
+++ frontend/src/components/ResponsiveDrawer.tsx
@@ -15,10 +15,12 @@ import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Avatar, Button, Menu, MenuItem, Tooltip } from '@mui/material';
+import { Avatar, Menu, MenuItem, Tooltip } from '@mui/material';
+import { useSession } from '../hooks/useSession';
+import { Link } from 'react-router-dom';
 
-const pages = ['학사정보', '공지사항', '대관신청'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settingsWithSession = ['로그아웃'];
+
 const drawerWidth = 240;
 
 interface Props {
@@ -34,16 +36,9 @@ export default function ResponsiveDrawer(props: React.PropsWithChildren<Props>) 
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  const { hasSession, signout } = useSession();
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
@@ -116,34 +111,48 @@ export default function ResponsiveDrawer(props: React.PropsWithChildren<Props>) 
               />
             </Typography>
           </Box>
+
           <Box sx={{ flexGrow: 1, justifyContent: 'flex-end', display: 'flex' }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            {hasSession ? (
+              <>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settingsWithSession.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : (
+              <Box display="flex" gap="20px">
+                <Link style={{ color: 'inherit' }} to="/signin">
+                  로그인
+                </Link>
+                <Link style={{ color: 'inherit' }} to="/signup">
+                  회원가입
+                </Link>
+              </Box>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
