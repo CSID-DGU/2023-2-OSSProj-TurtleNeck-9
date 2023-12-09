bc8c47e78fb8099516c7c737fa5c2db5aa4e812a
jjm2317 <wlaks2317@gmail.com>
2023-12-09 21:50:31 +0900

    feat: integrate signout

--- frontend/src/components/ResponsiveDrawer.tsx
+++ frontend/src/components/ResponsiveDrawer.tsx
@@ -44,6 +44,8 @@ export default function ResponsiveDrawer(props: React.PropsWithChildren<Props>) 
   };
 
   const handleCloseUserMenu = () => {
+    signout();
+    navigate('signin');
     setAnchorElUser(null);
   };
 
