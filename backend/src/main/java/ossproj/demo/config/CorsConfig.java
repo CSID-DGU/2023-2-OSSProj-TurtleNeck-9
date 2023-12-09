aa7580246a65061ec0d40dac24d4d62b88f41ab8
Seongjoon-K <jobcho6320@naver.com>
2023-12-09 23:24:17 +0900

    fix: security filter 수정

--- backend/src/main/java/ossproj/demo/config/CorsConfig.java
+++ backend/src/main/java/ossproj/demo/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
