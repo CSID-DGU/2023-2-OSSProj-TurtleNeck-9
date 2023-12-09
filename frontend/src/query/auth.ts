f8119058c5c90fea9a174a0edda32fbce2c9d68d
jjm2317 <wlaks2317@gmail.com>
2023-12-09 20:31:59 +0900

    feat: create signin hook

--- frontend/src/query/auth.ts
+++ frontend/src/query/auth.ts
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
-import { SignupPayload } from '../types/auth';
+import { SigninPayload, SignupPayload } from '../types/auth';
 
 const postSignup = async (payload: SignupPayload) => {
   const response = await axios.post('https://43.203.18.207:8080/users/signup', payload);
@@ -10,4 +10,14 @@ const postSignup = async (payload: SignupPayload) => {
 
 export const useSignupMutation = () => {
   return useMutation({ mutationFn: postSignup });
+};
+
+const postSignin = async (payload: SigninPayload) => {
+  const response = await axios.post('https://43.203.18.207:8080/users/login', payload);
+
+  return response.data;
+};
+
+export const useSigninMutation = () => {
+  return useMutation({ mutationFn: postSignin });
 };
