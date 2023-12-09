e85546845d392e17f14a84c6c20979b2be0dd502
jjm2317 <wlaks2317@gmail.com>
2023-12-09 20:02:11 +0900

    feat: create signup hook

---
+++ frontend/src/query/auth.ts
@@ -0,0 +1,13 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { SignupPayload } from '../types/auth';
+
+const postSignup = async (payload: SignupPayload) => {
+  const response = await axios.post('http://43.203.18.207:8080/users/signup', payload);
+
+  return response.data;
+};
+
+export const useSignupMutation = () => {
+  return useMutation({ mutationFn: postSignup });
+};
