52f95c1a6943740f8372945302c6b824e3586764
jjm2317 <wlaks2317@gmail.com>
2023-12-13 13:15:11 +0900

    feat: error handling

--- frontend/src/pages/time-table/index.tsx
+++ frontend/src/pages/time-table/index.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress, Container } from '@mui/material';
+import { Box, CircularProgress, Container, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import ResponsiveDrawer from '../../components/ResponsiveDrawer';
 import TimeTable from '../../components/TimeTable';
@@ -6,13 +6,14 @@ import { useLectureListQuery } from '../../query/lecture';
 
 const TimeTableDetail = () => {
   const { timeTableNumber } = useParams();
-  const { data } = useLectureListQuery(Number(timeTableNumber));
+  const { data, isError } = useLectureListQuery(Number(timeTableNumber));
 
   return (
     <Container>
       <ResponsiveDrawer>
         <Box display="flex" minHeight="400px" justifyContent="space-around" flexDirection="column" alignItems="center">
           {data ? <TimeTable lectureList={data} /> : <CircularProgress />}
+          {isError && <Typography>시간표 조회 중 에러가 발생했습니다. 새로고침 후 다시 시도해주세요.</Typography>}
         </Box>
       </ResponsiveDrawer>
     </Container>
